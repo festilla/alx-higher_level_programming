267e55207dbbb8ce9cfad7f02a2109eb990b8068
festilla <festusndame@gmail.com>
2023-11-29 01:24:52 -0800

    update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print(''.join(chr(i) for i in range(ord('a'), ord('z') + 1) if chr(i) not in {'q', 'e'}), end='')
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) != 'e' and chr(i) != 'q':
+        print('{:c}'.format(i), end='')
