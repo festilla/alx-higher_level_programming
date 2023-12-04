ff2a5492a273df75b9c9a48c6ae9d70cd3b719b5
festilla <festusndame@gmail.com>
2023-12-04 03:45:39 -0800

    update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@
 def analyze_sentence(sentence):
     result_data = ()
     if len(sentence) == 0:
-    result_data = 0, "None"
+       result_data = 0, "None"
     else:
-    result_data = len(sentence), sentence[0]
+       result_data = len(sentence), sentence[0]
     return result_data
