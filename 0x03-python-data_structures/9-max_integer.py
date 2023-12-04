eeb77afac9da33549a516a4e40c0c562ae14ecde
festilla <festusndame@gmail.com>
2023-12-04 03:49:56 -0800

    update

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,12 +2,11 @@
 
 def find_maximum_value(input_list=[]):
     if len(input_list) == 0:
-     return
-     maximum_value = input_list[0]
-     for i in range(1, len(input_list)):
+       return
+       maximum_value = input_list[0]
+       for i in range(1, len(input_list)):
      if maximum_value < input_list[i]:
-     maximum_value = input_list[i]
+       maximum_value = input_list[i]
      else:
-     continue
-     return maximum_value
-
+       continue
+       return maximum_value
